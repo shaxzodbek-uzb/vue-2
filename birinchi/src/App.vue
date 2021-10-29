ba48e295b1596d48df667012dffa97e7182eab69
Shaxzodbek <shaxzodbek.qambaraliyev@gmail.com>
2021-10-29 21:14:54 +0500

    2-dars

--- birinchi/src/App.vue
+++ birinchi/src/App.vue
@@ -1,25 +1,76 @@
 <template>
   <div id="app">
-    <UserList />
+    <p style="font-size: 20px;" class="qalin-text">Salom</p>
+    <input type="text" v-model="surename" />
+    <textarea cols="30" rows="10" v-model="desc"></textarea>
+
+    <table>
+      <tr>
+        <td>1</td>
+        <td>{{ full_name }}</td>
+      </tr>
+    </table>
+    <NameComponent name="Jasurbek" :surename="surename" v-if="false" />
+    <NameComponent name="Shaxzodbek" />
+    <NameComponent v-bind:name="item" v-for="(item, index) in names" :key="index" />
     <hr />
-    <UserList />
+    {{ desc }}
+    <div v-text="desc"></div>
+    <div v-html="desc"></div>
     <hr />
-    <UserList />
+    <div v-if="son == 1">Bir</div>
+    <div v-else>Ikki</div>
     <hr />
-    <UserList />
-    <hr />
-    <UserList />
-    <hr />
-    <UserList />
+    <div v-if="son == 1">Bir</div>
+    <div v-else-if="son == 2">Ikki</div>
+    <div v-else-if="son == 3">Uch</div>
+    <div v-else-if="son == 4">To'rt</div>
+    <div v-else-if="son == 5">Besh</div>
+    <div v-else>Bunday sonni bilmayman</div>
   </div>
 </template>
 
 <script>
-import UserList from "./components/Users.vue";
-
+// Bir komponentani boshqasi ichida ishlatish uchun
+// 1. import qilish kerak
+// 2. komponentani ro'yhatdan o'tkazish
+// 3. o'z nomi bilan foydalanish tag ko'rishida
+import NameComponent from "./components/Name";
 export default {
-  name: "App",
-  components: { UserList }
+  name: "AsosiyKomponenta",
+  components: { NameComponent },
+  props: {
+    // parentdan keladigan atributlarni qabul qiladi
+  },
+  data() {
+    // Componentaning reative o'zgaruvchilarini saqlash uchun
+    return {
+      name: "Shaxzodbek",
+      surename: "Abdurasulov",
+      names: ["Ibrohim", "Shahobiddin", "Anvar", "Dilshod"],
+      desc: "",
+      son: 7
+    };
+  },
+  computed: {
+    // boshqa ma'lumotlar o'zgarishiga qarab o'z qiymatini o'zgartiradi
+    full_name() {
+      return "Mr " + this.name;
+    }
+  },
+  methods: {
+    // komponentaga tegishli bo'lgan logik funksiyalar jamlanmasi
+    qoshish(a, b) {
+      return a + b;
+    }
+  }
 };
 </script>
 
+
+<style>
+/* istalgan css, sass, less... kodlarini kiritish mumkin */
+.qalin-text {
+  font-weight: bold;
+}
+</style>
---
+++ birinchi/src/components/Name.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>{{ full_name }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    surename: {
+      type: String,
+      default: ""
+    }
+  },
+  computed: {
+    full_name() {
+      return `Mr ${this.surename} ${this.name}`;
+    }
+  }
+};
+</script>
